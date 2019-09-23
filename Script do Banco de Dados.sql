f95f36b33383216fe077306201be81a4ee8938e8
Aluno Bandtec <github@alunos.bandtec.com.br>
2019-09-23 17:06:12 -0300

    Correção das tabelas no BD

--- Script do Banco de Dados.sql
+++ Script do Banco de Dados.sql
@@ -1,4 +1,4 @@
- create table cliente(
+ create table Cliente(
   idCliente int primary key,
   Nome varchar(30),
   CPF int (11),
@@ -7,7 +7,7 @@
   Cidade varchar(30),
   Estado varchar(2),
   Pais varchar(30),
-  fkProduto int, foreign key (fkProdutos) references curso(idProduto)
+  fkProduto int
  );
  
  create table Produto(
@@ -15,7 +15,7 @@
  Nome varchar(30),
  Reg_Umidade decimal (5,2),
  Reg_Temperatura decimal (4,2),
- fkPlanta int, foreign key (fkPlanta) references curso(idPlanta)
+ fkPlanta int
  );
  
  create table Planta(
@@ -24,9 +24,7 @@
  Temp_ideal decimal (4,2),
  umid_ideal decimal (5,2),
  informação varchar (200)
- )
+ );
  
- 
- 
- 
- 
+ alter table Produto add foreign key (fkPlanta) references Planta(idPlanta);
+ alter table Cliente add foreign key (fkProduto) references Produto(idProduto);
