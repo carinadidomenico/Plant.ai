1514ce5b00342bafb6ccd8c8ab3eeb74f27b9b55
carinadidomenico <carinadidomenico@gmail.com>
2019-11-19 21:21:28 -0200

    Layout dashboard

---
+++ projeto-site/public/dash/script.js
@@ -0,0 +1,25 @@
+var resposta = document.querySelector(".faq-resposta");
+var slide = document.querySelector(".slide");
+
+
+slide.addEventListener('click', function () {
+    if (resposta.style.height){
+        resposta.style.height = null;
+        resposta.style.padding = null;
+    } else {
+        resposta.style.height = "5rem";
+        resposta.style.padding = ".5rem 1rem";
+    }  
+})
+
+// for(var i = 0; i < slide.lenght; i++) {
+//     slide[i].addEventListener('click', function () {
+//         if (resposta[i].style.height){
+//             resposta[i].style.height = null;
+//             resposta[i].style.padding = null;
+//         } else {
+//             resposta[i].style.height = "5rem";
+//             resposta[i].style.padding = ".5rem 1rem";
+//         }  
+//     })
+// }
