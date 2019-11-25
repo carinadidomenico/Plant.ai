7b71b4fadc6caddecef50f3d5fff30955ed8f6ea
carinadidomenico <carinadidomenico@gmail.com>
2019-11-25 20:04:46 -0200

    Dashboard adicionando página plantas, perfil, faq

--- projeto-site/public/css e js site/dashboard.js
+++ projeto-site/public/css e js site/dashboard.js
@@ -1,26 +1,117 @@
-var resposta = document.querySelector(".faq-resposta");
-var slide = document.querySelector(".slide");
+var resposta = document.getElementsByClassName("faq-resposta");
+var slide = document.getElementsByClassName("slide");
 
 
+// FAQ
 
-slide.addEventListener('click', function () {
-    if (resposta.style.height){
-        resposta.style.height = null;
-        resposta.style.padding = null;
+// slide.addEventListener('click', function () {
+//     if (resposta.style.height){
+//         resposta.style.height = null;
+//         resposta.style.padding = null;
+//     } else {
+//         resposta.style.height = "5rem";
+//         resposta.style.padding = ".5rem 1rem";
+//     }  
+// })
+
+
+for (var i = 0; i < slide.length; i++) {
+    slide[i].addEventListener('click', function(){
+        for(var i = 0; i < resposta.length; i++) {
+            if(resposta[i].style.height) {
+                resposta[i].style.height = null;
+                resposta[i].style.padding = null;
+            } else {
+                resposta[i].style.height = "5rem";
+                resposta[i].style.padding = ".5rem 1rem"
+            }
+        }
+    });
+}
+
+
+// TABELA PLANTAS
+
+var listaPlantas = plantas;
+
+    // ORDEM ALFABÉTICA
+
+listaPlantas.tipos.sort((tipo1, tipo2) => {
+
+
+    if(tipo1.nome.toLowerCase() < tipo2.nome.toLowerCase()){
+        return -1
+    }
+    if(tipo1.nome.toLowerCase() > tipo2.nome.toLowerCase()){
+        return 1
     } else {
-        resposta.style.height = "5rem";
-        resposta.style.padding = ".5rem 1rem";
-    }  
+        return 0
+    } 
+    
 })
 
-// for(var i = 0; i < slide.lenght; i++) {
-//     slide[i].addEventListener('click', function () {
-//         if (resposta[i].style.height){
-//             resposta[i].style.height = null;
-//             resposta[i].style.padding = null;
-//         } else {
-//             resposta[i].style.height = "5rem";
-//             resposta[i].style.padding = ".5rem 1rem";
-//         }  
-//     })
-// }
+const insertTable = (tabela) => {
+    
+    let tabelas = ''
+    tabela.forEach(tipo => {
+        
+        tabelas += `
+        <table>
+            <thead>
+                <tr>
+                    <th>Infos</th>
+                    <th>${tipo.nome}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Descrição</td>
+                    <td>${tipo.descricao}</td>
+                </tr>
+                <tr>
+                    <td>Temperatura Mínima</td>
+                    <td>${tipo.tempMin}</td>
+                </tr>
+                <tr>
+                    <td>Temperatura Máxima</td>
+                    <td>${tipo.tempMax}</td>
+                </tr>
+                <tr>
+                    <td>Temperatura Ideal</td>
+                    <td>${tipo.tempIdeal}</td>
+                </tr>
+                <tr>
+                    <td>Umidade Mínima</td>
+                    <td>${tipo.umiMin}</td>
+                </tr>
+                <tr>
+                    <td>Umidade Máxima</td>
+                    <td>${tipo.umiMax}</td>
+                </tr>
+                <tr>
+                    <td>Umidade Ideal</td>
+                    <td>${tipo.umiIdeal}</td>
+                </tr>
+            </tbody>
+        </table>
+        `
+    });
+    return tabelas;
+}
+
+document.querySelector(".tabelas").innerHTML = insertTable(listaPlantas.tipos);
+
+
+// BUSCA DE PLANTAS
+
+var pesquisar = document.querySelector(".buscar-planta")
+
+pesquisar.addEventListener('keyup', function() {
+
+    let filtro = listaPlantas.tipos.filter ((item) => {
+        let searchInput = pesquisar.value.toLowerCase()
+        return item.nome.toLowerCase().includes(searchInput)
+    })
+
+    document.querySelector(".tabelas").innerHTML = insertTable(filtro)    
+})
